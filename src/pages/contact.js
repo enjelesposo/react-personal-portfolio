3a38573372c942dc06b2d64f668d1ffd9d261bb9
Angelica Esposo <73009213+enjelesposo@users.noreply.github.com>
2021-07-20 19:54:31 +0800

    Delete src/pages directory

--- src/pages/contact.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { NavbarContainer } from '../containers/navbar';
-
-export default function Contact() {
-    return <>
-        <NavbarContainer />
-        <h1>This is the contact page!</h1>
-    </>
-};
--- src/pages/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Home } from './home';
-export { default as About } from './about';
-export { default as Contact } from './contact';
